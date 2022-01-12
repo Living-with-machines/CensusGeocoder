c0655e26b9a2b0816ebe60b8fd17fb05c573365d
josh-rhodes <jrhodes@turing.ac.uk>
2022-01-12 15:58:29 +0000

    add parameters file and adjust to accept new parameter format

--- historic-census-gb-geocoder/historic_census_gb_geocoder.py
+++ historic-census-gb-geocoder/historic_census_gb_geocoder.py
@@ -1,21 +1,30 @@
 # Main script that combines other scripts
 import setupgeocoder
 from datetime import datetime
+import json
 
-year_list = [1901]
+with open('data/historic-census-gb-geocoder-params.json') as f:
+	geocode_parameters = json.load(f)
 
-start = datetime.now()
+for key, value in geocode_parameters.items():
+	if value == 'no':
+		continue
+	else:
 
-for year in year_list:
-	# Initiate census geocoder
-	census_geocoder = setupgeocoder.CensusGB_geocoder(year,'EW','testing')
-	print(vars(census_geocoder))
+		start = datetime.now()
 
-	os_roads, gb1900, icem, census_counties = census_geocoder.preprocessing()
-	dsh_output = census_geocoder.geocoding(icem,gb1900,os_roads,census_counties)
+		country = key[:2]
+		census_year = int(key.split('_')[1])
+		type = value
 
-	print(dsh_output['sh_id'].is_unique)
+		census_geocoder = setupgeocoder.CensusGB_geocoder(census_year,country,type)
+		print(vars(census_geocoder))
 
-	end_time = datetime.now() - start
-	print('Time to run: ',end_time.total_seconds()/60)
+		os_roads, gb1900, icem, census_counties = census_geocoder.preprocessing()
+		dsh_output = census_geocoder.geocoding(icem,gb1900,os_roads,census_counties)
 
+		# Retain for testing purposes for now
+		# print(dsh_output['sh_id'].is_unique)
+
+		end_time = datetime.now() - start
+		print('Time to run: ',end_time.total_seconds()/60)
