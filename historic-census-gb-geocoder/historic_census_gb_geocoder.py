69d0dfec3cfe1782aebd6d7e5aac0057a22c4419
josh-rhodes <jrhodes@turing.ac.uk>
2022-01-19 16:04:41 +0000

    add simple try/except

--- historic-census-gb-geocoder/historic_census_gb_geocoder.py
+++ historic-census-gb-geocoder/historic_census_gb_geocoder.py
@@ -10,21 +10,24 @@ for key, value in geocode_parameters.items():
 	if value['type'] == 'no':
 		continue
 	else:
+		try:
+			start = datetime.now()
 
-		start = datetime.now()
+			country = key.split('_')[0]
+			census_year = int(key.split('_')[1])
+			type = value['type']
 
-		country = key.split('_')[0]
-		census_year = int(key.split('_')[1])
-		type = value['type']
+			census_geocoder = setupgeocoder.CensusGB_geocoder(census_year,country,type)
+			print(vars(census_geocoder))
 
-		census_geocoder = setupgeocoder.CensusGB_geocoder(census_year,country,type)
-		print(vars(census_geocoder))
+			os_roads, gb1900, icem, census_counties = census_geocoder.preprocessing(value['use_existing_files'])
+			dsh_output = census_geocoder.geocoding(icem,gb1900,os_roads,census_counties)
 
-		os_roads, gb1900, icem, census_counties = census_geocoder.preprocessing(value['use_existing_files'])
-		dsh_output = census_geocoder.geocoding(icem,gb1900,os_roads,census_counties)
+			# Retain for testing purposes for now
+			# print(dsh_output['sh_id'].is_unique)
 
-		# Retain for testing purposes for now
-		# print(dsh_output['sh_id'].is_unique)
+			end_time = datetime.now() - start
+			print('Time to run: ',end_time.total_seconds()/60)
 
-		end_time = datetime.now() - start
-		print('Time to run: ',end_time.total_seconds()/60)
+		except:
+			print('An error occurred')
