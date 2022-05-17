d30ac7d63b3f0d7a2e503ac577b4560ca4f1f33b
josh-rhodes <jrhodes@turing.ac.uk>
2022-05-17 16:17:28 +0100

    change file paths for inputs

--- historic-census-gb-geocoder/historic_census_gb_geocoder.py
+++ historic-census-gb-geocoder/historic_census_gb_geocoder.py
@@ -3,7 +3,7 @@ import setupgeocoder
 from datetime import datetime
 import json
 
-with open('../inputs/historic-census-gb-geocoder-params.json') as f:
+with open('./inputs/historic-census-gb-geocoder-params.json') as f:
 	geocode_parameters = json.load(f)
 
 input_data_path = geocode_parameters["input_data_path"]
