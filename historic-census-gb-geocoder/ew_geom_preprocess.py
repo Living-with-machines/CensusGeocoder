799feb4d984bf822c66ee47425b226eaa6647842
josh-rhodes <jrhodes@turing.ac.uk>
2022-07-22 11:12:12 +0100

    add docstrings

--- historic-census-gb-geocoder/ew_geom_preprocess.py
+++ historic-census-gb-geocoder/ew_geom_preprocess.py
@@ -1,52 +1,64 @@
-import pandas as pd
 import geopandas as gpd
+import pandas as pd
 import pygeos as pg
-
-
-# gpd.options.use_pygeos = True
 
 
 def process_rsd_boundary_data(rsd_id_field, rsd_gis_config):
     """
-    Reads combined Parish / Registration Sub District (RSD) boundary data
+    Reads combined Registration Sub District (RSD) boundary data. Generates
+    correct boundaries for census year by dissolving on specified rsd_id_field.
 
     Parameters
     ----------
-    path_to_rsd_boundary_data : str, path object
-        Either the absolute or relative path to the combined Parish/RSD boundary file.
-    conparid: str
-        Column label for consistent parish ID, either 'conpar51-9' or 'conpar01-1'
-    cen: str
-        Column label for RSD id, takes format 'CEN_' followed by year, e.g. 'CEN_1901'.
-    new_id: pandas.series.name
-        Column label for new_id that combines conparid and cen; default 'new_id'
+    rsd_id_field : str
+        Name of rsd id field for census year, e.g. 'CEN_1851'.
+    rsd_gis_config: Dataclass
+        Dataclass containing parameters for reading RSD GIS Boundary data
+        for census year.
 
     Returns
     --------
-    geopandas.GeoDataFrame
-        A geopandas geodataframe containing the Parish/RSD Boundary data with a new id
-        column created from the conparid and cen id variables.
+    rsd_boundary: geopandas.GeoDataFrame
+        A geopandas geodataframe containing the RSD Boundary data for census year.
     """
 
-    print("Reading Registration Sub District (RSD) boundary data")
-
+    # Read one line of the shapefile to create list of 'unwanted columns'
     tmp_file = gpd.read_file(rsd_gis_config.filepath, rows=1)
     list_of_all_cols = tmp_file.columns.values.tolist()
     cols_to_keep = [rsd_id_field, tmp_file.geometry.name]
     unwanted_cols = [col for col in list_of_all_cols if col not in cols_to_keep]
 
-    par_rsd_boundary = gpd.read_file(
+    rsd_boundary = gpd.read_file(
         rsd_gis_config.filepath,
         ignore_fields=unwanted_cols,
         crs=rsd_gis_config.projection,
     )
 
-    par_rsd_boundary = par_rsd_boundary.dissolve(by=rsd_id_field).reset_index()
+    rsd_boundary = rsd_boundary.dissolve(by=rsd_id_field).reset_index()
 
-    return par_rsd_boundary
+    print("Processed Registration Sub District (RSD) boundary data")
+
+    return rsd_boundary
 
 
 def read_gis_to_icem(parish_icem_lkup_config, conparid):
+    """
+    Reads lookup table linking England and Wales parish boundary data to
+    I-CeM.
+
+    Parameters
+    ----------
+    parish_icem_lkup_config : Dataclass
+        Dataclass containing parameters for reading lookup table.
+    conparid: str
+        Consistent parish identifier for census year, e.g. 'conparid_51-91' for 1851
+        to 1891.
+
+    Returns
+    --------
+    gis_to_icem: pandas.DataFrame
+        A Pandas DataFrame containing the lookup table.
+    """
     list_of_cols = [parish_icem_lkup_config.ukds_id_field, conparid]
     gis_to_icem = pd.read_excel(
         parish_icem_lkup_config.filepath,
@@ -60,6 +72,33 @@ def read_gis_to_icem(parish_icem_lkup_config, conparid):
 def process_parish_boundary_data(
     parish_gis_config, ukds_lkuptbl, conparid, parish_icem_lkup_idfield,
 ):
+    """
+    Reads England and Wales parish boundary data. Merges parish boundary
+    data with lookup table to assign each boundary a 'parid'. Then dissolves
+    boundaries on the consistent parish identifier for census year. Returns
+    a geopandas.GeoDataFrame containing parish boundary data for census year
+    linkable to I-CeM.
+
+    Parameters
+    ----------
+    parish_gis_config : Dataclass
+        Dataclass containing parameters for reading Parish GIS Boundary data.
+    ukds_lkuptbl: pandas.DataFrame
+        Pandas DataFrame containing lookup table
+    conparid: str
+        Name of consistent parish identifier for census year
+    parish_icem_lkup_idfield: str
+        Unique id field that links lookup table to parish boundary data.
+
+
+    Returns
+    --------
+    par_boundary_conparid: geopandas.GeoDataFrame
+        A geopandas geodataframe containing parish boundary data for census year
+        dissolved using consistent parish id.
+        
+    """
+
     print("Reading Parish Boundary Data")
     tmp_file = gpd.read_file(parish_gis_config.filepath, rows=1)
     list_of_all_cols = tmp_file.columns.values.tolist()
@@ -93,6 +132,36 @@ def process_parish_boundary_data(
 
 
 def join_parish_rsd_boundary(par_boundary, rsd_boundary, conparid, rsd_id_field):
+    """
+    Merges parish boundary data with rsd boundary data to create new geometries -
+    some rsds are larger than consistent parish boundaries and vice versa. When rsd
+    is larger than a consistent parish boundary, the rsd will be split into smaller
+    units according to the consistent parish boundaries. Ditto for consistent parish
+    boundaries that are larger than rsds.
+
+    Dissolves on a temporary unique id produced by combining consistent parish
+    identifier and rsd id.
+
+    Parameters
+    ----------
+    par_boundary : geopandas.GeoDataFrame
+        A geopandas geodataframe containing parish boundary data for census year.
+    rsd_boundary : geopandas.GeoDataFrame
+        A geopandas geodataframe containing rsd boundary data for census year.
+    conparid: str
+        Name of consistent parish identifier for census year
+    rsd_id_field : str
+        Name of rsd id field for census year, e.g. 'CEN_1851'.
+
+
+    Returns
+    --------
+    par_rsd_boundary: geopandas.GeoDataFrame
+        A geopandas geodataframe containing parish/rsd boundary data for census year.
+    geom_blocking_cols: list
+        List of columns for geo-blocking when running string comparisons.
+        
+    """
 
     print("Joining Parish Boundary and RSD Boundary")
     # print(par_boundary['geometry'].is_valid.all())
@@ -115,19 +184,17 @@ def join_parish_rsd_boundary(par_boundary, rsd_boundary, conparid, rsd_id_field)
 
 def read_rsd_dictionary(rsd_dictionary_config):
     """
-    Read the RSD Dictionary lookup file for the appropriate census year.
+    Read the RSD Dictionary lookup file for census year.
 
     Parameters
     ----------
-    rsd_dictionary: str
-        Path to rsd dictionary file.
-    field_dict: dictionary
-        Dictionary with field values.
+    rsd_dictionary_config: Dataclass
+        Dataclass containing parameters for reading rsd dictionary.
 
     Returns
     -------
     pandas.DataFrame
-        A pandas dataframe containing the RSD Dictionary lookup table.
+        A pandas dataframe containing the RSD Dictionary lookup table for census year.
     """
 
     rsd_variables = [
