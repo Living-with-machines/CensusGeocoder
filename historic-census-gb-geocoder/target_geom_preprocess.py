1477f5ab2fca4d81830060263deed47cc9cece22
josh-rhodes <jrhodes@turing.ac.uk>
2022-07-22 11:58:11 +0100

    add docstrings

--- historic-census-gb-geocoder/target_geom_preprocess.py
+++ historic-census-gb-geocoder/target_geom_preprocess.py
@@ -5,10 +5,17 @@ import pathlib
 
 
 def set_geom_files(geom_config):
-    """Create list of file(s) containing target geometry data
+    """Accepts a directory or single filepath and creates a list of file(s)
+    containing target geometry data.
+
+    Parameters
+    -------
+
+    geom_config: Dataclass
+        Dataclass containing parameters for target geometry data.
 
     Returns
-    ----------
+    -------
     geom_files: list
         List containing file(s) of target geometry data.
     """
@@ -28,7 +35,35 @@ def set_geom_files(geom_config):
 def process_raw_geo_data(
     geom_name, boundary_data, geom_config, census_params, output_dir
 ):
-    """"""
+    """Process target geometry data. Save processed geometry data to
+    file. Returns dataframe with geometry data removed (to reduce size)
+    and since geometry data is not needed for linking to census data. Also
+    returns the new unique identifier for geometry data.
+
+    Parameters
+    -------
+
+    geom_name: str
+        Name of the target geometry data.
+    boundary_data: geopandas.GeoDataFrame
+        Historic boundary data used to assign target geometries to historic
+        boundary units.
+    geom_config: Dataclass
+        Dataclass containing parameters for target geometry data.
+    census_params: Dataclass
+        Dataclass containing parameters for census year.
+    output_dir: str
+        Path to write processed geometry data.
+
+    Returns
+    -------
+    target_df_processed_small: pandas.DataFrame
+        Pandas dataframe containing target address data and assigned
+        historic boundary ids.
+    new_uid: str
+        Name of unique identifier column created from target geometry name
+        and census year.
+    """
     print("*" * 100)
     print(f"Reading {geom_name} geometry data")
     cols_to_keep = geom_config.data_fields.list_cols()
@@ -39,10 +74,9 @@ def process_raw_geo_data(
 
     if geom_config.file_type == "shp":
         target_gdf = read_shp(filelist, cols_to_keep, geom_config)
-        # print(target_gdf)
 
     elif geom_config.file_type == "csv":
-        target_df = read_csv(geom_config, cols_to_keep, filelist)
+        target_df = read_csv(filelist, cols_to_keep, geom_config)
         if geom_config.geometry_format == "coords":
             target_gdf = process_coords(target_df, geom_config)
         elif geom_config.geometry_format == "wkt":
@@ -51,7 +85,6 @@ def process_raw_geo_data(
     target_gdf = (
         target_gdf.dropna().copy()
     )  # need to check the effect this is having by not using subset any more.
-    # print(target_gdf)
 
     if geom_config.geom_type == "line":
         target_gdf_processed = process_linstring(
@@ -62,16 +95,8 @@ def process_raw_geo_data(
         target_gdf_processed = process_point(
             target_gdf, boundary_data, geom_config, new_uid
         )
-        # print(target_gdf_processed.info())
 
     target_gdf_processed = parse_address(target_gdf_processed, geom_config)
-
-    print(target_gdf_processed.info())
-
-    # print(
-    #     f"{output_dir}/{geom_name}_{census_params.year}{geom_config.output_params.file_type}"
-    # )
-    # print(geom_config.output_params.driver)
 
     if target_gdf_processed.crs != geom_config.output_params.crs:
         target_gdf_processed = target_gdf_processed.to_crs(
@@ -83,23 +108,63 @@ def process_raw_geo_data(
         driver=geom_config.output_params.driver,
         crs=geom_config.output_params.crs,
     )
-    target_gdf_processed_small = target_gdf_processed.drop(
-        columns=[target_gdf_processed.geometry.name]
+    target_df_processed_small = pd.DataFrame(
+        target_gdf_processed.drop(columns=[target_gdf_processed.geometry.name])
     )
-    print(target_gdf_processed_small.info())
 
-    return target_gdf_processed_small, new_uid
+    return target_df_processed_small, new_uid
 
 
-def drop_outside_country(target_gdf, new_id):
-    """Drop roads outside country (i.e. with
-    no associated parish info from the union"""
+def drop_outside_country(target_gdf, tmp_id):
+    """Drop target geometries with no associated historic boundary data
+    after join.
 
-    tmp = target_gdf.dropna(subset=new_id).copy()
-    return tmp
+    Parameters
+    ----------
+
+    target_gdf: geopandas.GeoDataFrame
+        Geopandas geodataframe containing target geometry data with associated historic
+        boundary info.
+    tmp_id: str
+        Name of temporary id column.
+
+    Returns
+    -------
+    target_gdf:  geopandas.GeoDataFrame
+        Geopandas geodataframe containing subset of target geometry data only
+        with associated historic boundary info.
+    """
+
+    target_gdf = target_gdf.dropna(subset=tmp_id).copy()
+
+    return target_gdf
 
 
 def process_linstring(line_string_gdf, boundary_data, geom_config, new_uid):
+    """Overlays historic boundary data with target geometry linestrings to create
+    new linestrings. For example, if a linestring crosses more than one boundary,
+    the linestring is split into two. Returns a geopandas GeoDataFrame of these
+    new geometries.
+
+    Parameters
+    ----------
+
+    ling_string_gdf: geopandas.GeoDataFrame
+        Geopandas geodataframe containing target geometry data in a linestring format.
+    boundary_data: geopandas.GeoDataFrame
+        Historic boundary data used to assign target geometries to historic
+        boundary units.
+    geom_config: Dataclass
+        Dataclass containing parameters for target geometry data.
+    new_uid: str
+        Name of unique identifier column created in `process_raw_geo_data` function.
+    
+
+    Returns
+    -------
+    tmp4:  geopandas.GeoDataFrame
+        Geopandas geodataframe containing processed linestring geometries.
+    """
     tmp = line_string_gdf.dissolve(by=geom_config.data_fields.uid_field, as_index=False)
     print(tmp)
     tmp2 = gpd.overlay(tmp, boundary_data, how="identity", keep_geom_type=True)
@@ -121,6 +186,30 @@ def process_linstring(line_string_gdf, boundary_data, geom_config, new_uid):
 
 
 def process_point(point_gdf, boundary_data, geom_config, new_uid):
+    """Conducts spatial join between historic boundary data and
+    target geometry points to assign each point to historic boundary.
+    Returns a geopandas GeoDataFrame of these points with associated historic
+    boundary info.
+
+    Parameters
+    ----------
+
+    point_gdf: geopandas.GeoDataFrame
+        Geopandas geodataframe containing target point geometry data.
+    boundary_data: geopandas.GeoDataFrame
+        Historic boundary data used to assign target geometries to historic
+        boundary units.
+    geom_config: Dataclass
+        Dataclass containing parameters for target geometry data.
+    new_uid: str
+        Name of unique identifier column created in `process_raw_geo_data` function.
+    
+
+    Returns
+    -------
+    tmp3:  geopandas.GeoDataFrame
+        Geopandas geodataframe containing processed point geometries.
+    """
     tmp = gpd.sjoin(
         left_df=point_gdf, right_df=boundary_data, predicate="intersects", how="inner"
     ).drop(columns=["index_right"])
@@ -138,7 +227,25 @@ def process_point(point_gdf, boundary_data, geom_config, new_uid):
 
 
 def parse_address(target_gdf, geom_config):
+    """Process address data in target geometry in preparation
+    for comparison with I-CeM data. Converts addresses to uppercase.
+    If a standardisation file has been specified, then regex replacements
+    are made using that file.
 
+    Parameters
+    ----------
+
+    target_gdf: geopandas.GeoDataFrame
+        Geopandas geodataframe containing target geometry data.
+    geom_config: Dataclass
+        Dataclass containing parameters for target geometry data.
+
+    Returns
+    -------
+    target_gdf:  geopandas.GeoDataFrame
+        Geopandas geodataframe containing target geometry data with processed
+        addresses.
+    """
     target_gdf[geom_config.data_fields.address_field] = target_gdf[
         geom_config.data_fields.address_field
     ].str.upper()
@@ -161,6 +268,24 @@ def parse_address(target_gdf, geom_config):
 
 
 def read_shp(filelist, cols_to_keep, geom_config):
+    """Reads target geometry from a single shapefile or multiple
+    shapefiles. Returns target geometry in geopandas GeoDataFrame.
+
+    Parameters
+    ----------
+
+    filelist: list
+        List of filepaths.
+    cols_to_keep: list
+        Columns from target geometry data to read.
+    geom_config: Dataclass
+        Dataclass containing parameters for target geometry data.
+
+    Returns
+    -------
+    target_gdf:  geopandas.GeoDataFrame
+        Geopandas geodataframe containing target geometry data.
+    """
     tmp_file = gpd.read_file(filelist[0], rows=1)
     list_of_all_cols = tmp_file.columns.values.tolist()
 
@@ -173,7 +298,7 @@ def read_shp(filelist, cols_to_keep, geom_config):
                     target_shp,
                     ignore_fields=unwanted_cols,
                     crs=geom_config.projection,
-                    rows=1000,
+                    rows=1000,  # create sample data and remove.
                 )
                 for target_shp in filelist
             ]
@@ -183,7 +308,25 @@ def read_shp(filelist, cols_to_keep, geom_config):
     return target_gdf
 
 
-def read_csv(geom_config, cols_to_keep, filelist):
+def read_csv(filelist, cols_to_keep, geom_config):
+    """Reads target geometry from a single delimited file or multiple
+    delimited files. Returns target geometry data in pandas DataFrame.
+
+    Parameters
+    ----------
+
+    filelist: list
+        List of filepaths.
+    cols_to_keep: list
+        Columns from target geometry data to read.
+    geom_config: Dataclass
+        Dataclass containing parameters for target geometry data.
+
+    Returns
+    -------
+    target_df:  pandas.DataFrame
+        Pandas dataframe containing target geometry data.
+    """
     target_df = pd.concat(
         [
             pd.read_csv(
@@ -191,7 +334,7 @@ def read_csv(geom_config, cols_to_keep, filelist):
                 sep=geom_config.sep,
                 encoding=geom_config.encoding,
                 usecols=cols_to_keep,
-                nrows=1000,
+                nrows=1000,  # create sample data and remove.
             )
             for csv_file in filelist
         ]
@@ -200,6 +343,22 @@ def read_csv(geom_config, cols_to_keep, filelist):
 
 
 def process_coords(target_df, geom_config):
+    """Processes coordindates in a pandas dataframe. Returns
+    a geopandas GeoDataFrame with geometry data and specified crs.
+
+    Parameters
+    ----------
+
+    target_df:  pandas.DataFrame
+        Pandas dataframe containing target geometry data.
+    geom_config: Dataclass
+        Dataclass containing parameters for target geometry data.
+
+    Returns
+    -------
+    target_gdf:  geopandas.GeoDataFrame
+        Geopandas geodataframe containing target geometry data.
+    """
     target_gdf = gpd.GeoDataFrame(
         target_df,
         geometry=gpd.points_from_xy(
@@ -214,9 +373,27 @@ def process_coords(target_df, geom_config):
 
 
 def process_wkt(target_df, geom_config):
+    """Processes wkt strings in a pandas dataframe. Returns
+    a geopandas GeoDataFrame with geometry data and specified crs.
+
+    Parameters
+    ----------
+
+    target_df:  pandas.DataFrame
+        Pandas dataframe containing target geometry data.
+    geom_config: Dataclass
+        Dataclass containing parameters for target geometry data.
+
+    Returns
+    -------
+    target_gdf:  geopandas.GeoDataFrame
+        Geopandas geodataframe containing target geometry data.
+    """
     target_gdf = gpd.GeoDataFrame(
         target_df,
-        geometry=gpd.GeoSeries.from_wkt(target_df["wkt"]),
+        geometry=gpd.GeoSeries.from_wkt(
+            target_df[geom_config.data_fields.geometry_field]
+        ),
         crs=geom_config.projection,
     )
     return target_gdf
