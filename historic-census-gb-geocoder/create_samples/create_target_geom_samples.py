9e54401a1028cd7420efa9fca261d6545a9b515d
josh-rhodes <jrhodes@turing.ac.uk>
2022-08-02 12:44:40 +0100

    adjust paths

--- historic-census-gb-geocoder/create_samples/create_target_geom_samples.py
+++ historic-census-gb-geocoder/create_samples/create_target_geom_samples.py
@@ -4,18 +4,22 @@ import geopandas as gpd
 
 """Create GB1900 sample"""
 
+gb1900_p = pathlib.Path("data/input/target_geoms/gb1900")
+gb1900_p_sample = pathlib.Path(gb1900_p, "sample")
+pathlib.Path(gb1900_p_sample).mkdir(parents=True, exist_ok=True)
+
+
 gb1900 = pd.read_csv(
-    "data/input/target_geoms/gb1900_gazetteer_complete_july_2018.csv",
-    sep=",",
-    encoding="utf-16",
+    gb1900_p / "gb1900_gazetteer_complete_july_2018.csv", sep=",", encoding="utf-16",
 )
+
 
 sample_size = round(len(gb1900) * 0.1)
 
 gb1900_sample = gb1900.sample(sample_size, random_state=1)
 
 gb1900_sample.to_csv(
-    f"data/input/sample/target_geoms/gb1900_gazetteer_complete_july_2018.csv",
+    gb1900_p_sample / "gb1900_gazetteer_complete_july_2018_sample.csv",
     sep=",",
     encoding="utf-16",
     index=False,
@@ -24,7 +28,12 @@ gb1900_sample.to_csv(
 """Create OS Open Roads Sample"""
 
 geom_files = []
-p = pathlib.Path("data/input/sample/target_geoms/oproad_essh_gb-2/data")
+p = pathlib.Path("data/input/target_geoms/oproad_essh_gb-2/data")
+
+p_sample = pathlib.Path(p, "sample")
+pathlib.Path(p_sample).mkdir(parents=True, exist_ok=True)
+
+
 for file_p in p.iterdir():
     if "RoadLink.shp" in str(file_p):
         geom_files.append(str(file_p))
@@ -37,6 +46,4 @@ for file_p in p.iterdir():
 
         os_roads_sample = os_roads.sample(sample_size, random_state=1)
         # print(os_roads_sample)
-        os_roads_sample.to_file(
-            f"data/input/sample/target_geoms/oproad_essh_gb-2/data/{grid}_RoadLink.shp"
-        )
+        os_roads_sample.to_file(p_sample / f"{grid}_RoadLink.shp")
