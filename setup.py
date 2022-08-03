e5ab7ac4c503f32a6b875e02758d048f97403c1a
josh-rhodes <jrhodes@turing.ac.uk>
2022-08-03 17:33:33 +0100

    add dask, pyyaml, change python to 3.8

--- setup.py
+++ setup.py
@@ -5,30 +5,28 @@ setuptools.setup(
     version="n/a",
     description="Geocodes Historic Great British Census Data 1851-1911",
     author=u"josh-rhodes",
-    #author_email="",
+    # author_email="",
     license="MIT License",
     keywords=["britain", "streets", "census", "living with machines"],
-    long_description = open('README.md', encoding="utf8").read(),
-    long_description_content_type = 'text/markdown',
-    zip_safe = False,
+    long_description=open("README.md", encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    zip_safe=False,
     url="",
     download_url="",
-    packages = setuptools.find_packages(),
-    include_package_data = True,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
     platforms="",
-    python_requires='>=3.7',
+    python_requires=">=3.8",
     install_requires=[
         "numpy>=1.21.5",
         "pandas>=1.3.4",
         "Shapely>=1.8.0",
         "scikit-learn>=1.0.1",
-		"geopandas>=0.9.0",
-		"pygeos>=0.10.2",
-		"recordlinkage>=0.14",
-		"rapidfuzz>=1.5.0"
-    ]
-
-
-
-    
+        "geopandas>=0.9.0",
+        "pygeos>=0.10.2",
+        "recordlinkage>=0.14",
+        "rapidfuzz>=1.5.0",
+        "pyyaml>=6.0",
+        "dask>=2022.5.0",
+    ],
 )
