e5bce20c49e0c1cc5a4b120393e784537f78ebb4
josh-rhodes <jrhodes@turing.ac.uk>
2024-10-02 17:28:31 +0100

    finish documentation for geocode.py

--- censusgeocoder/geocode.py
+++ censusgeocoder/geocode.py
@@ -4,42 +4,40 @@ import utils
 
 
 class GeoCode:
-    """
-    A class to geocode census data.
+    """A class to geocode census data.
 
     Attributes
     ----------
 
-
-    census_data: pandas.DataFrame
+    census_data: pd.DataFrame
         DataFrame containing census data.
 
     census_geocode_field: str
-        Column name of pandas.Series containing text (e.g. Addresses) to geocode in census_data.
+        Name of pd.Series in `census_data` containing text (e.g. Addresses) to geocode.
 
     census_indexfield: str
-        Column name of pandas.Series in census_data DataFrame containing index values.
+        Name of pd.Series in `census_data` containing index values.
 
-    target_geometry_data: pandas.DataFrame
-        DataFrame containing target geometry data (no geometries stored).
+    target_geometry_data: pd.DataFrame
+        DataFrame containing target geometry data (no geometries stored - see `geometry.Geometry.create_tgforlinking()`).
 
     target_geometry_geocode_field: str
-        Column name of pandas.Series containing field in target_geometry_data to
-        perform string comparison with census_geocode_field.
+        Name of pd.Series in `target_geometry_data` to
+        perform string comparison with `census_geocode_field`.
 
     target_geometry_indexfield:
-        Column name of pandas.Series in target_geometry_data DataFrame containing index values.
+        Name of pd.Series in `target_geometry_data` DataFrame containing index values.
 
     census_block: list
-        List of column name(s) of pandas.Series in census_data DataFrame to limit
-        string comparisons between census_data and target_geometry_data.
+        List of name(s) of pd.Series in `census_data` to limit
+        string comparisons between `census_data` and `target_geometry_data`.
 
     target_geom_block: list
-        List of column name(s) of pandas.Series in target_geometry_data DataFrame
-        to limit string comparisons between census_data and target_geometry_data.
+        List of name(s) of pd.Series in `target_geometry_data`
+        to limit string comparisons between `census_data` and `target_geometry_data`.
 
     comparers: dict
-        Dictionary of comparison functions and corresponding labels to pass to utils.rapidfuzzy_wratio_comparer.
+        Dictionary of comparison functions and corresponding labels to pass to `utils.rapidfuzzy_wratio_comparer()`.
 
     sim_thresh: int
         Threshold to filter string similarity comparison scores; possible matches
@@ -50,22 +48,51 @@ class GeoCode:
         an alignment length equal to or higher than align_thresh.
 
     comparison_method: str
-        Name of comparison method to calculate final comparison score in _calc_finalscore()
+        Name of comparison method to calculate final comparison score in `_calc_finalscore()`.
 
     final_score_field: str
-        Label to set column name of pandas.Series containing final comparison scores.
+        Label to set name of pd.Series containing final comparison scores.
 
-    Methods
+    cand_links: pd.MultiIndex
+        A pd.MultiIndex of two records, one from `census_data` and one from `target_geometry_data`.
+
+    tgt_rslts: pd.Dataframe
+        A pd.DataFrame of all matches between census and target geometry data that meet given thresholds.
+
+    rslts_dict: dict
+        A dictionary of 3 geocoded pd.DataFrames, where the keys are the names of the outputs and the values are pd.DataFrame. E.g.
+
+        ```{"matches": pd.DataFrame,
+            "competing_matches": pd.DataFrame,
+            "matches_lq": pd.DataFrame,
+        }
+        ```
+
+    Notes
     -------
 
-    create_candidate_links()
-        Creates candidate links based on geo-blocking to pass to the string comparison function.
-    compare()
-        Performs fuzzy string matching between candidate links and filters results by similarity and alignment thresholds.
-    process_results()
-        Links target matches to census_data and target_geometry data; calculates final scores.
+    Some details on the private methods:
 
+    `_create_candidate_links()`
+        Create candidate links based on geo-blocking to pass to the string comparison
+        function `_compare`. Returns a pd.MultiIndex of pairs of records, one from `census_data` and one
+        from `target_geometry_data`.
 
+    `_compare()`
+        Performs fuzzy string matching between candidate links returned by `create_candidate_links()`.
+        Returns a pd.DataFrame of all possible matches after filtering on thresholds.
+
+    `_process_results()`
+        Filters `target_results` to produce 3 sets of results. Returns dictionary containing these 3 dataframes.
+
+    _set_comparisons()
+        Add comparison algorithms specified in configuration files to instance of `recordlinkage.Compare`
+
+    `_filterbythreshold()`
+        Filters possible matches by threshold similarity score specified in configuration files.
+
+    `_calc_finalscore()`
+        Calculates final comparison scores, returns matches_all `pd.DataFrame` with scores added.
     """
 
     def __init__(
@@ -85,49 +112,52 @@ class GeoCode:
         final_score_field: str,
     ) -> None:
         self.census_data = census_data
+        self.census_geocode_field = census_geocode_field
         self.census_indexfield = census_indexfield
-        self.target_geometry_indexfield = target_geometry_indexfield
+
         self.target_geometry_data = target_geometry_data
+        self.target_geometry_geocode_field = target_geometry_geocode_field
+        self.target_geometry_indexfield = target_geometry_indexfield
+
         self.census_block = census_block
         self.target_geom_block = target_geom_block
+
         self.comparers = comparers
         self.sim_thresh = sim_thresh
-        self.comparison_method = comparison_method
-        self.census_geocode_field = census_geocode_field
-        self.target_geometry_geocode_field = target_geometry_geocode_field
-        self.final_score_field = final_score_field
         self.align_thresh = align_thresh
+        self.comparison_method = comparison_method
+        self.final_score_field = final_score_field
 
-        self.census_data = self.census_data.set_index(self.census_indexfield)
+        self.census_data = self.census_data.set_index(
+            self.census_indexfield
+        )  # set index of census data to specified index field
 
         self.target_geometry_data = self.target_geometry_data.set_index(
             self.target_geometry_indexfield
-        )
+        )  # set index of target geometry data to specified index field
 
         self.cand_links = self.create_candidate_links()
 
         self.tgt_rslts = self.compare(self.cand_links)
 
-        # self.lnked, self.lnked_dup, self.lnked_left, = self.process_results(
-        #     self.tgt_rslts
-        # )
         self.rslts_dict = self.process_results(self.tgt_rslts)
 
-    def create_candidate_links(
+    def _create_candidate_links(
         self,
-    ):
+    ) -> pd.MultiIndex:
         """Create candidate links based on geo-blocking to pass to the string comparison
-        function. Returns a pandas.MultiIndex of two records, one from the census and one
-        from the target geometry dataset.
+        function `_compare`. Returns a pd.MultiIndex of pairs of records, one from `census_data` and one
+        from `target_geometry_data`.
 
         Returns
         -------
         target_candidate_links: pandas.MultiIndex
             A pandas MultiIndex of two records, one from census_data and one from target_geometry_data.
+
         """
 
         if self.census_data.empty or self.target_geometry_data.empty:
-            print("No census or target geom data for this county")
+            print("No census or target geom data - therefore no candidate links")
             target_candidate_links = pd.MultiIndex(
                 levels=[
                     [],
@@ -135,7 +165,7 @@ class GeoCode:
                 codes=[
                     [],
                 ],
-            )  # check this doesn't break the code
+            )  # add blank pd.MultiIndex so that code runs even if there are no valid candidates to link, e.g. subsets of the census data with no valid addresses.
         else:
             targetgeom_indexer = recordlinkage.Index()
             blocking_fields_l = self.census_block
@@ -150,31 +180,33 @@ class GeoCode:
 
         return target_candidate_links
 
-    def compare(
+    def _compare(
         self,
         target_candidate_links,
-    ):
-        """
-        Performs fuzzy string matching between candidate links identified by create_candidate_links().
-        Returns a dataframe of all possible matches after filtering on thresholds.
+    ) -> pd.DataFrame:
+        """Performs fuzzy string matching between candidate links returned by `create_candidate_links()`.
+        Returns a DataFrame of all possible matches after filtering on thresholds.
 
         Parameters
         ----------
 
-        target_candidate_links: pandas.MultiIndex
-            A pandas MultiIndex of two records, one from census_data and one from target_geometry_data.
+        target_candidate_links: pd.MultiIndex
+            pd.MultiIndex of record pairs, one from `census_data` and one from `target_geometry_data`.
 
         Returns
         -------
-        target_results: pandas.Dataframe
-            A pandas dataframe of all matches between census and target geometry data.
+        target_results: pd.Dataframe
+            pd.DataFrame of all matches between `census_data` and `target_geometry_data`.
+
         """
         if target_candidate_links.empty:
             print("No candidate links to compare")
-            target_results = pd.DataFrame()
+            target_results = (
+                pd.DataFrame()
+            )  # add blank pd.DataFrame so that code runs even if there are no valid candidates to link, e.g. subsets of the census data with no valid addresses.
 
         else:
-            target_comparison = recordlinkage.Compare()  # Set up comparison
+            target_comparison = recordlinkage.Compare()
 
             target_comparison = self._set_comparisons(
                 target_comparison,
@@ -189,96 +221,97 @@ class GeoCode:
 
         return target_results.reset_index()
 
-    def process_results(
+    def _process_results(
         self,
         target_results,
-    ):
-        """Processes target results by selecting best batch using a combination of
-        fuzzy string matching score and tf-idf weighting. Candidate links with more
-        than one strong match are stored in a duplicates table. Returns matches and
-        duplicate matches.
+    ) -> dict:
+        """Filters `target_results` to produce 3 sets of results:
+        1. Highest scoring matches with no competing matches of equal quality (`matches`)
+        2. Highest scoring matches with more than 1 competing match of equal quality (`competing_matches`)
+        3. Remaining `target_results` not included in 1 or 2; matches above threshold but not highest quality (`matches_lq`)
+
 
         Parameters
         ----------
-        census: pandas.DataFrame
-            A pandas dataframe containing census data.
 
-        target_geom_data: geopandas.GeoDataFrame
-            A geopandas dataframe containing target geometry data.
-
-        census_params: Dataclass
-            Dataclass containing parameters for census year.
-
-        geom_config: Dataclass
-            Dataclass containing parameters for target geometry data.
-
-        new_uid: str
-            Name of unique identifier column created from target geometry name
-            and census year.
+        target_results: pd.DataFrame
+             pd.DataFrame of all matches between census and target geometry data that meet given thresholds.
 
         Returns
         -------
-        linked: pandas.Dataframe
-            A pandas dataframe of high-quality matches between target geometry data
-            and census.
 
-        linked_duplicates: pandas.Dataframe
-            A pandas dataframe of duplicate matches between target geometry data and census
-            (where there is >1 match of equal, high quality).
+        A dictionary of 3 sets of results in the format:
 
-        linked_leftovers: pandas.DataFrame
-            A pandas DataFrame of comparison results meeting threshold criteria but not included in linked or linked_duplicates.
+        ```{"matches": pd.DataFrame,
+            "competing_matches": pd.DataFrame,
+            "matches_lq": pd.DataFrame,
+        }
+        ```
         """
 
         if target_results.empty:
             print(f"No matches or no matches meeting filter thresholds")
-            linked = pd.DataFrame()
-            linked_duplicates = pd.DataFrame()
-            linked_leftovers = pd.DataFrame()
+            matches = pd.DataFrame()
+            competing_matches = pd.DataFrame()
+            matches_lq = pd.DataFrame()
         else:
-            linked_all = target_results.merge(
+            matches_all = target_results.merge(
                 self.census_data[self.census_geocode_field],
                 left_on=self.census_data.index.name,
-                right_index=True,  # sort this out
+                right_index=True,
             ).merge(
                 self.target_geometry_data[[self.target_geometry_geocode_field]],
                 left_on=self.target_geometry_data.index.name,
                 right_index=True,
             )
 
-            linked_all = self._calc_finalscore(linked_all)
+            matches_all = self._calc_finalscore(matches_all)
 
-            linked_all_maxonly = linked_all[
-                linked_all[self.final_score_field]
-                == linked_all.groupby(self.census_data.index.name)[
+            matches_all_maxonly = matches_all[
+                matches_all[self.final_score_field]
+                == matches_all.groupby(self.census_data.index.name)[
                     self.final_score_field
                 ].transform("max")
             ]
 
-            linked = linked_all_maxonly.drop_duplicates(
+            matches = matches_all_maxonly.drop_duplicates(
                 subset=[self.census_data.index.name], keep=False
             )
-            linked_duplicates = linked_all_maxonly[
-                linked_all_maxonly.duplicated(
+            competing_matches = matches_all_maxonly[
+                matches_all_maxonly.duplicated(
                     subset=[self.census_data.index.name], keep=False
                 )
             ]
 
-            linked_leftovers = linked_all[
-                (linked_all.index.isin(linked.index) == False)
-                & (linked_all.index.isin(linked_duplicates) == False)
+            matches_lq = matches_all[
+                (matches_all.index.isin(matches.index) == False)
+                & (matches_all.index.isin(competing_matches) == False)
             ]
 
         return {
-            "linked": linked,
-            "linked_duplicates": linked_duplicates,
-            "linked_leftovers": linked_leftovers,
+            "matches": matches,
+            "competing_matches": competing_matches,
+            "matches_lq": matches_lq,
         }
 
     def _set_comparisons(
         self,
         target_comparison,
-    ):
+    ) -> recordlinkage.Compare:
+        """Add comparison algorithms specified in configuration files to instance of `recordlinkage.Compare`
+
+        Parameters
+        ----------
+
+        target_comparison: `recordlinkage.Compare`
+            Instance of `recordlinkage.Compare`
+
+        Returns
+        ----------
+        target_comparison: `recordlinkage.Compare`
+            Instance of `recordlinkage.Compare` with comparison algorithms added.
+
+        """
         for comparer_method, comparer_label in self.comparers.items():
             target_comparison.add(
                 utils.rapidfuzzy_wratio_comparer(
@@ -291,27 +324,56 @@ class GeoCode:
 
         return target_comparison
 
-    def _calc_finalscore(self, linked_all):
-        if self.comparison_method == "1911_bespoke":
-            linked_all[list(self.comparers.values())[2]] = linked_all.groupby(
-                self.census_data.index.name
-            )[list(self.comparers.values())[2]].transform("rank", ascending=False)
+    def _filterbythreshold(self, target_results) -> pd.DataFrame:
+        """Filters possible matches by threshold similarity score specified in configuration files.
 
-        linked_all[self.final_score_field] = linked_all[
-            list(self.comparers.values())
-        ].prod(axis="columns")
+        Parameters
+        ----------
 
-        return linked_all.copy()
+        target_results: `pd.DataFrame`
+            'pd.DataFrame` containing all possible matches between census data and target geometry data.
 
-    def _filterbythreshold(self, target_results):
-        if self.sim_thresh != None:
+        Returns
+        ----------
+        target_results: `pd.DataFrame`
+            `pd.DataFrame` of matches that have similarity or align scores equal to or greater than specified thresholds.
+
+        """
+        if self.sim_thresh is not None:
             target_results = target_results[
                 target_results[list(self.comparers.values())[0]] >= self.sim_thresh
             ]
 
-        if self.align_thresh != None:
+        if self.align_thresh is not None:
             target_results = target_results[
                 target_results[list(self.comparers.values())[1]] >= self.align_thresh
             ]
 
         return target_results.copy()
+
+    def _calc_finalscore(self, matches_all) -> pd.DataFrame:
+        """Calculates final comparison scores, adding these to the matches_all dataframe.
+
+        Parameters
+        ----------
+
+        mathes_all: `pd.DataFrame`
+            'pd.DataFrame` containing possible matches between census data and target geometry data.
+
+        Returns
+        ----------
+        matches_all: `pd.DataFrame`
+            `pd.DataFrame` containing all possible matches between census data and target geometry data with added comparison scores.
+
+        """
+
+        if self.comparison_method == "1911_bespoke":
+            matches_all[list(self.comparers.values())[2]] = matches_all.groupby(
+                self.census_data.index.name
+            )[list(self.comparers.values())[2]].transform("rank", ascending=False)
+
+        matches_all[self.final_score_field] = matches_all[
+            list(self.comparers.values())
+        ].prod(axis="columns")
+
+        return matches_all.copy()
